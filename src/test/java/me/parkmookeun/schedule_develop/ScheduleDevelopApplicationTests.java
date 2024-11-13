3afd723cd35dbabdfa481ccd4bb528b00bac02c4
parkmookeun <sohappynow12@naver.com>
2024-11-13 19:50:05 +0900

    git init

---
+++ src/test/java/me/parkmookeun/schedule_develop/ScheduleDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.parkmookeun.schedule_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
